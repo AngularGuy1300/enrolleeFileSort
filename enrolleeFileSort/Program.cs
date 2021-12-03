4657ee5005b396536c2add3dd7bdd88891456765
Sean Lee <slee1300@hotmail.com>
2021-12-03 08:23:05 -0500

    Add project files.

---
+++ enrolleeFileSort/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace EnrolleeFileSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read the CSV into a generic list
+            List<Enrollee> newMemberList = File.ReadAllLines("C:\\Users\\Public\\enrolleeList.csv")
+                                          .Skip(1)
+                                          .Select(m => Utilities.parseFile(m))
+                                          .ToList();
+
+            //First get the highest version #s
+            List<Enrollee> filteredbyVersion = new List<Enrollee>();
+
+            var queryMemberList =
+                from member in newMemberList
+                group member by member.userId into memberGroup
+                orderby memberGroup.Key
+                select memberGroup;
+
+            foreach (var nameGroup in queryMemberList)
+            {
+                Console.WriteLine($"Key: {nameGroup.Key}");
+                foreach (var enrollee in nameGroup)
+                {
+
+                    Console.WriteLine($"\t{enrollee.userId}, {enrollee.firstName}, {enrollee.lastName}," +
+                        $" {enrollee.insuranceCompany}, {enrollee.versionNumber}");
+                }
+
+                Enrollee latest = nameGroup.OrderByDescending(p => p.versionNumber).FirstOrDefault();
+                filteredbyVersion.Add(latest);
+            }                
+
+            //Group the list by Insurance Company Name
+            var queryInsuranceCompanies =
+            from member in filteredbyVersion//newMemberList
+            group member by member.insuranceCompany into newGroup
+            orderby newGroup.Key
+            select newGroup;
+
+            
+            foreach (var insuranceGroup in queryInsuranceCompanies)
+            {
+                List<Enrollee> finalVersion = new List<Enrollee>();
+                Console.WriteLine($"Key: {insuranceGroup.Key}");
+                foreach (var insuranceMember in insuranceGroup)
+                { 
+
+                    Console.WriteLine($"\t{insuranceMember.userId}, {insuranceMember.firstName}, {insuranceMember.lastName}," +
+                        $" {insuranceMember.insuranceCompany}, {insuranceMember.versionNumber}");
+                    
+                    finalVersion.Add(insuranceMember);  
+                }
+
+                //Create a file for each insurance company
+                Utilities.ExportCsv(finalVersion, insuranceGroup.Key.ToString());
+            }
+        }
+    }
+}
---
+++ enrolleeFileSort/Utilities.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EnrolleeFileSort
+{
+    public static class Utilities
+    {
+        public static Enrollee parseFile(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+            Enrollee member = new Enrollee();
+            member.userId = Convert.ToString(values[0]);
+            member.firstName = Convert.ToString(values[1]);
+            member.lastName = Convert.ToString(values[2]);
+            member.versionNumber = Convert.ToInt32(values[3]);
+            member.insuranceCompany = Convert.ToString(values[4]);
+            return member;
+        }
+
+        public static void ExportCsv<T>(List<T> genericList, string fileName)
+        {
+            var sb = new StringBuilder();
+            var basePath = "C:\\Output";
+            var finalPath = Path.Combine(basePath, fileName + ".csv");
+            var header = "";
+            var info = typeof(T).GetProperties();
+            if (!File.Exists(finalPath))
+            {
+                var file = File.Create(finalPath);
+                file.Close();
+                foreach (var prop in typeof(T).GetProperties())
+                {
+                    header += prop.Name + "; ";
+                }
+                header = header.Substring(0, header.Length - 2);
+                sb.AppendLine(header);
+                TextWriter sw = new StreamWriter(finalPath, true);
+                sw.Write(sb.ToString());
+                sw.Close();
+            }
+            foreach (var obj in genericList)
+            {
+                sb = new StringBuilder();
+                var line = "";
+                foreach (var prop in info)
+                {
+                    line += prop.GetValue(obj, null) + "; ";
+                }
+                line = line.Substring(0, line.Length - 2);
+                sb.AppendLine(line);
+                TextWriter sw = new StreamWriter(finalPath, true);
+                sw.Write(sb.ToString());
+                sw.Close();
+            }
+        }
+    }
+}
